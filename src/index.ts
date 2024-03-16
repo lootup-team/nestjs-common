a85bc19d3070bcdf2d43c7f0d4e929fdeba05c95
gm50x <getuliomagela@outlook.com>
2024-03-16 20:07:56 -0300

    adds barrels

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ export * from './logger/anonymizer';
 export * from './logger/log-exception.filter';
 export * from './logger/log-http.middleware';
 export * from './logger/logger.config';
+
+export * from './common/compression.config';
+export * from './common/cors.config';
+export * from './common/helmet.config';
+export * from './common/outbound-http-tracing.config';
+export * from './common/route-prefix.config';
+export * from './common/versioning.config';
