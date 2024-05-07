5d6cd687fe73d7275c71810cad8ea5a3d8986036
gm50x <getuliomagela@outlook.com>
2024-05-07 09:34:07 -0300

    fix: sets the factory class method name

--- src/common.builder.ts
+++ src/common.builder.ts
@@ -4,4 +4,5 @@ import { CommonModuleOptions } from './common.options';
 export const { MODULE_OPTIONS_TOKEN, ConfigurableModuleClass } =
   new ConfigurableModuleBuilder<CommonModuleOptions>()
     .setClassMethodName('forRoot')
+    .setFactoryMethodName('createOptions')
     .build();
